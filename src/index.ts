a8c4aa1b0e3fdd46d1e008cd6cf872ef19350678
bhoehne <bhoehne@tutanota.com>
2022-02-09 22:49:12 -0300

    desafio feito

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ import helmet from "helmet";
 
 dotenv.config();
 
+export type Aluno = {
+	nome: string;
+	idade: number;
+	nota: number;
+}
+
 /**
  * App Variables
  */
@@ -39,5 +45,30 @@ app.listen(PORT, () => {
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
-	
+	class Aluno {
+		nome: string;
+		idade: number;
+		nota: number;
+
+		constructor(nome: string, idade: number, nota: number) {
+			this.nome = nome;
+			this.idade = idade;
+			this.nota = nota;
+		}
+	}
+
+	let aluno1 = new Aluno('Bruno', 34, 2)
+	let aluno2 = new Aluno('Renato', 34, 8)
+	let aluno3 = new Aluno('Eliel', 34, 10)
+
+	const alunos: Array<Aluno> = [];
+
+	alunos.push(aluno1, aluno2, aluno3)
+
+	let somaDasNotas = 0
+	alunos.forEach(_=>{
+		somaDasNotas += _.nota
+	})
+
+	console.log(`A soma da nota dos Ubuntus postulanes ao cargo de Node Master é ${somaDasNotas}`)
 });
